7de2e8137e29ab004ce4ca6e3624f993548cdaff
ssouza <ssouza@isightpartners.com>
2014-04-07 20:56:26 +0200

    Added maven support

--- playground/src/main/java/com/stevesouza/App.java
+++ playground/src/main/java/com/stevesouza/App.java
@@ -1,13 +1,49 @@
 package com.stevesouza;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    private static String urlStr = "https://www.virustotal.com/intelligence/hunting/notifications-feed/?key=9e799068d917ea16744988d059cb156d60e2a09d879954d1a8865e865965b7d4";
+
+
+
+    public static void main( String[] args ) throws IOException {
+        urlWithOpenStream();
+        urlWithConnection();
+    }
+
+    private static void urlWithOpenStream() throws IOException {
+        URL url = new URL(urlStr);
+
+//        URLConnection yc = oracle.openConnection();
+//        BufferedReader in = new BufferedReader(new InputStreamReader(
+//                yc.getInputStream()));
+        BufferedReader input = new BufferedReader(new InputStreamReader(new URL(urlStr).openStream()));
+        String inputLine;
+        while ((inputLine = input.readLine()) != null)
+            System.out.println(inputLine);
+
+        input.close();
+    }
+
+    private static void urlWithConnection() throws IOException {
+        URL url = new URL("http://example.com");
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        System.out.println(connection.getContent());
+
+        int code = connection.getResponseCode();
+        System.err.println(code);
     }
 }
