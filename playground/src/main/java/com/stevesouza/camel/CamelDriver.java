6e4763fac834387fa8be65e297893d8f613820c1
Steve Souza <steve@stevesouza.com>
2014-04-13 10:19:10 +0200

    Experiments with "direct:routename" routes in camel.

--- playground/src/main/java/com/stevesouza/camel/CamelDriver.java
+++ playground/src/main/java/com/stevesouza/camel/CamelDriver.java
@@ -1,18 +1,26 @@
 package com.stevesouza.camel;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.*;
 import org.apache.camel.impl.DefaultCamelContext;
 
 /**
- * Created by stevesouza on 4/12/14.
+ * Program that launches various camel tests.
  */
 public class CamelDriver {
 
     public static void main(String[] ags) throws Exception {
         CamelContext camel = new DefaultCamelContext();
         camel.addRoutes(new FileCopyRouteBuilder());
+        camel.addRoutes(new MessageToFileRouteBuilder());
+        ProducerTemplate template = camel.createProducerTemplate();
+
         camel.start();
+
+        // Send to a specific queue
+        template.sendBodyAndHeader("direct:messagetofile", "<hello>world!</hello>", "filename", "helloworld.txt");
+        template.sendBodyAndHeader("direct:messagetofile", "<hello>steve!</hello>", "filename", "hellosteve.txt");
+
+
         Thread.sleep(10000);
         camel.stop();
     }
---
+++ playground/src/main/java/com/stevesouza/camel/MessageToFileRouteBuilder.java
@@ -0,0 +1,19 @@
+package com.stevesouza.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+
+
+/* Camel route builder that copies files from one directory to another and leaves original file in place
+ */
+public class MessageToFileRouteBuilder extends RouteBuilder {
+    public MessageToFileRouteBuilder() {
+    }// body=${body}
+
+    @Override
+    public void configure() throws Exception {
+from("direct:messagetofile").
+        log("messageid=${id}, myheader=${headers.myheader}, allheaders=${headers}").
+                process(new MessagePeeker()).
+                to("file:/Users/stevesouza/gitrepo/testproject/playground/src/main/resources/data/out/?fileName=${headers.filename}");
+    }
+}
