6217b1668141512ce2ee77d1f224cbb9f71eeee8
Steve Souza <steve@stevesouza.com>
2014-05-01 22:24:38 +0200

    python scripts experimentations

---
+++ python/jsontest.py
@@ -0,0 +1,10 @@
+import json
+
+mylist = ["steven", "thomas", "souza"]
+print mylist
+print  json.dumps([1, 'simple', 'list'])
+#write to file
+# json.dump(x, file)
+
+# from json file to json
+# x = json.load(f)
---
+++ python/printfile.py
@@ -0,0 +1,6 @@
+# read the file into a string and print it
+
+with open ("big.txt", "r") as myfile:
+    data=myfile.read()
+    
+print data    
