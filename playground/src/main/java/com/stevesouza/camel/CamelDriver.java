0320142ef4cee221cf83478cadadec08e1cec3ef
Steve Souza <steve@stevesouza.com>
2014-05-28 21:41:25 +0200

    Forgot to commit CamelDriver

--- playground/src/main/java/com/stevesouza/camel/CamelDriver.java
+++ playground/src/main/java/com/stevesouza/camel/CamelDriver.java
@@ -21,47 +21,37 @@ public class CamelDriver {
 
     public static void main(String[] ags) throws Exception {
         ApplicationContext context = new ClassPathXmlApplicationContext("camelSpringApplicationContext.xml");
-        System.out.println("*****"+context);
-        System.out.println("*****"+context.getClass());
-        System.out.println("*****"+context.getParent());
+        //normal way of getting camel context but i have it defined in camelSpringApplicationContext.xml
+        // CamelContext camel = new DefaultCamelContext();
+        // CamelContext camel = new SpringCamelContext(context);
         CamelContext camel = (CamelContext) context.getBean("myCamelContext");
-        System.out.println("****routes="+camel.getRoutes());
 
-
-
-        //CamelContext camel = new DefaultCamelContext();
- //       CamelContext camel = new SpringCamelContext(context);
         // shows up in jmx with this context name instead of camel-1
         camel.setNameStrategy(new DefaultCamelContextNameStrategy("steves_camel_driver"));
-        camel.setTracing(true);
+        // camel.setTracing(true);  // set in camelSpringApplicationContext.xml
         camel.addStartupListener(new StartupListener() {
             @Override
             public void onCamelContextStarted(CamelContext context, boolean alreadyStarted) throws Exception {
                 System.out.println("Seeing when startup strategy is called.  CamelContext=" + context);
             }
        });
-//        camel.addRoutes(new FileCopyRouteBuilder());
-//        camel.addRoutes(new MessageToFileRouteBuilder());
-//        camel.addRoutes(new PojoToJsonToFileRouteBuilder());
-//        camel.addRoutes(new FileToJsonToPojoRouteBuilder());
-//        camel.addRoutes(new PojoToJsonToFileRouteBuilderXstream());
-//        camel.addRoutes(new FileToJsonToPojoRouteBuilderXstream());
 
         ProducerTemplate template = camel.createProducerTemplate();
 
-        camel.start();
+        // if camel wasn't started in the spring config xml file with autoStartup="true" then we would do the following:
+        //  camel.start();
         MyAutowiredClass myAutowiredClass = (MyAutowiredClass) context.getBean("myComponent");
         myAutowiredClass.getSlowerMethod();
 
 
         // Send to a specific queue
-        template.sendBodyAndHeader("direct:messagetofile", "<hello>world!</hello>", "filename", "WOOHOOhelloworld.txt");
-        template.sendBodyAndHeader("direct:messagetofile", "<hello>steve!</hello>", "filename", "WOOHOOhellosteve.txt");
-    //    template.sendBody("direct:personsname", new PersonsName("steve","souza"));
+        template.sendBodyAndHeader("direct:messagetofile", "<hello>world!</hello>", "filename", "helloworld.txt");
+        template.sendBodyAndHeader("direct:messagetofile", "<hello>steve!</hello>", "filename", "hellosteve.txt");
+        template.sendBody("direct:personsname", new PersonsName("steve","souza"));
         template.sendBody("direct:personsname_xstream", new PersonsName("joel","souza"));
 
 
-        Thread.sleep(10000);
+        Thread.sleep(5000);
         camel.stop();
         Utils.displayJamon();
 
