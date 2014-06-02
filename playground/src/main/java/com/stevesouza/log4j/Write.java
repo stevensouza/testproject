1c1c5e7a9bdf1154615ec4bc50826441802fe409
ssouza <ssouza@isightpartners.com>
2014-06-02 20:13:25 +0200

    played with log4j rollingfileappender to see how it rolls over files.

---
+++ playground/src/main/java/com/stevesouza/log4j/Write.java
@@ -0,0 +1,23 @@
+package com.stevesouza.log4j;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by ssouza on 6/2/14.
+ */
+public class Write {
+    private static final Logger LOG = Logger.getLogger(Write.class);
+
+    // test file rollover
+    public static void main(String[] args)  {
+        StringBuilder sb=new StringBuilder();
+        for (int i=0;i<1024;i++) {
+            sb.append('a');
+        }
+
+        for (int i=0;i<1024*5;i++) {
+            LOG.info("line "+i+" "+sb.toString());
+        }
+
+    }
+}
