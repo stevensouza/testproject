548619452d20308e09972d486cd04434f1a93cbe
Steve Souza <steve@stevesouza.com>
2014-08-06 22:25:43 +0200

    php

--- php/first.php
+++ php/first.php
@@ -1,4 +1,11 @@
- <?php 
+ <?php
+  $myvar = "HELLO";
  Echo "Hello, World!";
  echo get_include_path();
+ echo toLower($myvar);
+
+ function toLower($delme) {
+     return strtolower($delme)."hi!!!";
+ }
+
  ?> 
