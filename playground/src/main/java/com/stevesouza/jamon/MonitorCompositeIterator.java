ccba135f65f6964c66cbd43a89c30af9121f84f7
Steve Souza <steve@stevesouza.com>
2014-08-16 16:51:29 +0200

    experiments for jamon

---
+++ playground/src/main/java/com/stevesouza/jamon/MonitorCompositeIterator.java
@@ -0,0 +1,62 @@
+package com.stevesouza.jamon;
+
+import com.jamonapi.Monitor;
+import com.jamonapi.MonitorComposite;
+
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Class that allows for iterating a collection that contains multiple instances of MonitorComposites and iterates
+ * through them as if they were one MonitorComposite.  A sort of composite/collection of MonitorComposites.
+ *
+ * Created by stevesouza on 8/16/14.
+ */
+    public class MonitorCompositeIterator implements Iterator<Monitor> {
+    private Iterator<MonitorComposite> iter;
+
+    private MonitorComposite currentMonitorComposite;
+    private int index=-1;
+
+    public MonitorCompositeIterator(Set<MonitorComposite> monitorComposites) {
+        iter = monitorComposites.iterator();
+        currentMonitorComposite = iter.next();
+    }
+
+    @Override
+    public boolean hasNext() {
+      boolean hasMore;
+      if (index < currentMonitorComposite.getNumRows() - 1) {
+        hasMore = true;
+      } else {
+        hasMore = setNextMonitorComposite();
+      }
+
+      index++;
+      return hasMore;
+    }
+
+    private boolean setNextMonitorComposite() {
+        index = -1;
+        if (iter.hasNext()) {
+          currentMonitorComposite = iter.next();
+          return currentMonitorComposite.getNumRows() > 0;
+        }
+
+        return false;
+    }
+
+    @Override
+    public Monitor next() {
+       return currentMonitorComposite.getMonitors()[index];
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("Remove is not supported in this class");
+    }
+
+    public MonitorComposite getCurrentMonitorComposite() {
+        return currentMonitorComposite;
+    }
+}
---
+++ playground/src/test/java/com/stevesouza/jamon/MonitorCompositeIteratorTest.java
@@ -0,0 +1,71 @@
+package com.stevesouza.jamon;
+
+import com.jamonapi.MonitorComposite;
+import com.jamonapi.MonitorFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+
+public class MonitorCompositeIteratorTest {
+
+    @Before
+    public void setUp() throws Exception {
+        MonitorFactory.reset();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        MonitorFactory.reset();
+    }
+
+    @Test
+    public void testWorksWithOneComposite() throws Exception {
+        MonitorFactory.start("hello").stop();
+        MonitorFactory.start("world").stop();
+        MonitorComposite monitorComposite = MonitorFactory.getRootMonitor();
+        Set<MonitorComposite> set = new HashSet<MonitorComposite>();
+        set.add(monitorComposite);
+        MonitorCompositeIterator iter = new MonitorCompositeIterator(set);
+        int size = size(iter);
+
+        assertThat(size).isEqualTo(monitorComposite.getNumRows());
+    }
+
+    private int size(MonitorCompositeIterator iter) {
+        int size=0;
+        while (iter.hasNext()) {
+            size++;
+        }
+        return size;
+    }
+
+    @Test
+    public void testWorksWithMultipleComposites() throws Exception {
+        MonitorFactory.start("hello").stop();
+        MonitorFactory.start("world").stop();
+        MonitorComposite monitorComposite1 = MonitorFactory.getRootMonitor();
+        MonitorFactory.reset();
+
+        MonitorFactory.start("hello").stop();
+        MonitorFactory.start("world").stop();
+        MonitorFactory.add("page", "counter", 1);
+        MonitorComposite monitorComposite2 = MonitorFactory.getRootMonitor();
+
+        Set<MonitorComposite> set = new HashSet<MonitorComposite>();
+        set.add(monitorComposite1);
+        set.add(monitorComposite2);
+
+        MonitorCompositeIterator iter = new MonitorCompositeIterator(set);
+        int size = size(iter);
+
+        assertThat(size).isEqualTo(monitorComposite1.getNumRows()+monitorComposite2.getNumRows());
+
+    }
+}
