a1b4902c47bd551d8c1a71d100684d65f2147189
Steve Souza <steve@stevesouza.com>
2015-01-07 11:40:44 +0100

    java8 - starting with java lambdas

---
+++ playground/src/main/java/com/stevesouza/java8/Lambdas.java
@@ -0,0 +1,19 @@
+package com.stevesouza.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by stevesouza on 1/7/15.
+ */
+public class Lambdas {
+
+    public static void firstLambda() {
+        List features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+        features.forEach(n -> System.out.println(n));
+    }
+
+    public static void main(String[] args) {
+        firstLambda();
+    }
+}
