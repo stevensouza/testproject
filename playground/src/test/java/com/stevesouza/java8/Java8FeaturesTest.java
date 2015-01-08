f4dfb57db6515906b65cef7be2125b5697867e99
Steve Souza <steve@stevesouza.com>
2015-01-08 13:45:06 +0100

    java8 features - added tests

--- playground/src/test/java/com/stevesouza/java8/Java8FeaturesTest.java
+++ playground/src/test/java/com/stevesouza/java8/Java8FeaturesTest.java
@@ -4,7 +4,11 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.anyOf;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,6 +27,13 @@ public class Java8FeaturesTest {
 
         IntStream stream=IntStream.iterate(1, i->i+1).limit(10);
         stream.forEach(System.out::println);
+
+        stream=IntStream.iterate(1, i->i+1).limit(10);
+        stream.forEachOrdered(System.out::println);
+
+        // i think i read that ordered processing takes away the ability to run in parallel.
+        stream=IntStream.iterate(1, i->i+1).limit(10);
+        stream.parallel().forEachOrdered(System.out::println);
     }
     @Test
     public void testCount() {
@@ -63,10 +74,35 @@ public class Java8FeaturesTest {
         stream=IntStream.of(1, 2, 3, 4, 5);
         assertThat(stream.filter(i-> i%2==0).count()).isEqualTo(2);
 
+        stream=IntStream.of(1, 2, 3, 4, 5);
+        IntPredicate predicate = i->i%2==0;
+        assertThat(stream.filter(predicate).count()).isEqualTo(2);
+
         stream=IntStream.of(1, 2, 3, 4, 50);
         assertThat(stream.filter(value-> value==50).max().getAsInt()).isEqualTo(50);
 
         stream=IntStream.of(1, 2, 3, 4, 5);
         assertThat(stream.parallel().count()).isEqualTo(5);
+
+        // includes endpoints so has elements 1..5
+        stream=IntStream.rangeClosed(1, 5);
+        assertThat(stream.average().getAsDouble()).isEqualTo(3);
+
+        DoubleStream dstream=DoubleStream.of(1,2,3);
+        assertThat(dstream.count()).isEqualTo(3);
+
+        LongStream lstream=LongStream.builder().add(1).add(2).build();
+        assertThat(lstream.count()).isEqualTo(2);
+
+        // getting a stream from a collection
+        Stream<String> stream1 = stringList.stream();
+        assertThat(stream1.distinct().count()).isEqualTo(4);
+
+        stream1 = Stream.of("hi", "mom", "X");
+        assertThat(stream1.max((String s1, String s2)->s1.compareToIgnoreCase(s2)).get()).isEqualTo("X");
+
+        stream1 = Stream.of("a", "hi", "mom", "X");
+        assertThat(stream1.min((s1, s2)->s1.compareToIgnoreCase(s2)).get()).isEqualTo("a");
+
     }
 }
