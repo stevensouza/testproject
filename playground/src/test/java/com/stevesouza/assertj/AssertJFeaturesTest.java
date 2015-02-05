d745009b3d9dc5dbc8bcbc38589d32b9af1b4766
Steve Souza <steve@stevesouza.com>
2015-02-05 13:05:00 +0100

    assertj - added playground area for testing different assertions

---
+++ playground/src/test/java/com/stevesouza/assertj/AssertJFeaturesTest.java
@@ -0,0 +1,59 @@
+package com.stevesouza.assertj;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.StringReader;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.*;
+import java.util.regex.Pattern;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * examples for assertj:
+ *  https://github.com/joel-costigliola/assertj-examples/tree/master/assertions-examples/src/test/java/org/assertj/examples
+ */
+public class AssertJFeaturesTest {
+
+    @Test
+    public void testDescribedAs() {
+        // test will 'fail' but only print the messages as the exceptions are gobbled.
+        try {
+            assertThat(true).describedAs("My describedAs assertion message").isFalse();
+        } catch (AssertionError e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testAs() {
+        // Same as describedAs, but as is a key word in groovy so the preferred approach is to use describedAs
+        try {
+            assertThat(true).as("My as assertion message").isFalse();
+        } catch (AssertionError e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    @Test
+    public void testDescribedAsWithArgs() {
+        // describedAs formatting follows:
+        //   http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html
+        try {
+            int myNum=123;
+            String myName="steve";
+            // note %d could also be used for the number instead of %s
+            assertThat(true).describedAs("My describedAs assertion message with args name='%s', num=%s", myName, myNum).isFalse();
+        } catch (AssertionError e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
