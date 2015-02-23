a2e8ec12d4b2fdeb2410e81f0371d198616730cc
Steve Souza <steve@stevesouza.com>
2015-02-23 19:52:11 +0200

    aspectj - unkinded (within, withincode, this)

---
+++ aspectj/aspectj_compiletime/src/main/java/com/stevesouza/aspectj/javastyle/unkinded/various/MyAspect.java
@@ -0,0 +1,39 @@
+package com.stevesouza.aspectj.javastyle.unkinded.various;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+/**
+ * Aspect that advise field sets and gets.
+ *
+ * For example:
+ *  set:  obj.var="hi";
+ *  get:  System.out.println(obj.var);
+ *  get&set:  obj.var1=obj.var;
+ *
+ */
+@Aspect
+public class MyAspect  {
+
+    // note should be able to do this with just various, however with jdk 8 it gives byte code errors.
+    // I originally tried cflow(execution(* MyClass.myMethod(..))) however it gives stack errors.  The following
+    // is a work around that does the same thing.
+    @Before("within(MyClass) && execution(* *(..))")
+    public void myAdvice1(JoinPoint.StaticPart joinPoint) {
+        System.out.println("  Before myAdvice1 "+joinPoint.getKind()+": "+joinPoint);
+    }
+
+    // note the above will have main(..) executed whereas 'this' won't
+    @Before("this(MyClass) && execution(* *(..))")
+    public void myAdvice2(JoinPoint.StaticPart joinPoint) {
+        System.out.println("  Before myAdvice2 "+joinPoint.getKind()+": "+joinPoint);
+    }
+
+    // traces any direct calls in myMethod().  it doesn't track methods called by the methods that it calls.
+    @Before("withincode(public void MyClass.myMethod())")
+    public void myAdvice3(JoinPoint.StaticPart joinPoint) {
+        System.out.println("  Before myAdvice3 "+joinPoint.getKind()+": "+joinPoint);
+    }
+
+}
---
+++ aspectj/aspectj_compiletime/src/main/java/com/stevesouza/aspectj/javastyle/unkinded/various/MyClass.java
@@ -0,0 +1,38 @@
+package com.stevesouza.aspectj.javastyle.unkinded.various;
+
+
+/**
+ * Created by stevesouza on 2/4/15.
+ */
+public class MyClass  {
+
+    private String fname;
+
+    public MyClass(String fname) {
+        this.fname=fname;
+    }
+
+    public void myMethod() {
+        myMethod1();
+        myMethod3();
+        "steve".equals("steve");
+    }
+
+    private void myMethod1() {
+        myMethod2();
+    }
+
+    private void myMethod2() {
+        System.out.println(Math.abs(-100));
+    }
+
+    private void myMethod3() {
+    }
+
+    public static void main(String[] args) {
+        MyClass m = new MyClass("steve");
+        System.out.println("ObjectId="+m);
+        System.out.println(m.fname);
+        m.myMethod();
+    }
+}
