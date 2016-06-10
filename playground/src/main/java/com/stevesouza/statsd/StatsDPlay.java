b9398b7501d5378b5c286826a9b229e7ebad2660
Steve Souza <steve@stevesouza.com>
2016-06-10 19:04:08 +0200

    Did test to see if method names would work in statsd.  It doesn't work well due to method names have "(( ." characters in them.
    Graphite and statsd don't work well with these.

--- playground/src/main/java/com/stevesouza/statsd/StatsDPlay.java
+++ playground/src/main/java/com/stevesouza/statsd/StatsDPlay.java
@@ -10,8 +10,8 @@ import com.timgroup.statsd.StatsDClient;
 import com.timgroup.statsd.NonBlockingStatsDClient;
 
 /**
-  private static final StatsDClient statsd = new NonBlockingStatsDClient("my.prefix", "statsd-host", 8125);
-
+ * To install StatsD and graphite I followed this links directions.  You simply download a script and run it!
+ * http://markswanderingthoughts.nl/post/116538045615/getting-statsd-up-and-running-on-ubuntu-1404-in
 * @author stevesouza
  */
 public class StatsDPlay {
@@ -22,8 +22,14 @@ public class StatsDPlay {
     // due to lost udp packets
     for (int i=0;i<10;i++) {
         statsdClient.incrementCounter("mycounter");
+        statsdClient.incrementCounter("testperiod.mycounter");
         statsdClient.recordGaugeValue("myguage", 100);
         statsdClient.recordExecutionTime("mytimer", 25);
+        
+        // unfortunately stasd and graphite don't allow "() .".  Parens are simply removed, " " is relaced with "_". "."
+        // does works like a directory (heirarchy indicator).  So unfortunately method names don't work well in statsd.
+        statsdClient.recordExecutionTime("execution(void com.stevesouza.jdk.JdkHelloWorld.urlWithConnection(String))", 50);
+        
         // measures unique values...
         statsdClient.recordSetEvent("myset", "value1");
         statsdClient.recordSetEvent("myset", "value1");
