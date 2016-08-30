2d52b96ae086742055361fa1d3d9b0b4a5e01300
Steve Souza <steve@stevesouza.com>
2016-08-30 17:13:41 +0200

    commented out code that isn't compiling

--- aspectj/aspectj_compiletime/src/main/java/com/stevesouza/aspectj/javastyle/nativestyle/structure/ExtraInfo.java
+++ aspectj/aspectj_compiletime/src/main/java/com/stevesouza/aspectj/javastyle/nativestyle/structure/ExtraInfo.java
@@ -7,15 +7,15 @@ public interface ExtraInfo {
     public void setExtraInfo(String extraInfo);
     public String getExtraInfo();
 
-    static aspect ExtraInfoAspect {
-        private String ExtraInfo.extraInfo;
-
-        public void ExtraInfo.setExtraInfo(String extraInfo) {
-            this.extraInfo = extraInfo;
-        }
-
-        public String ExtraInfo.getExtraInfo() {
-            return this.extraInfo;
-        }
-    }
+//    static aspect ExtraInfoAspect {
+//        private String ExtraInfo.extraInfo;
+//
+//        public void ExtraInfo.setExtraInfo(String extraInfo) {
+//            this.extraInfo = extraInfo;
+//        }
+//
+//        public String ExtraInfo.getExtraInfo() {
+//            return this.extraInfo;
+//        }
+//    }
 }
