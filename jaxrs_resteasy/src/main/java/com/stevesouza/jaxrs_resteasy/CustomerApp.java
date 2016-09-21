4526b29da0ce008aba37f1b073dd12f339446cac
Steve Souza <steve@stevesouza.com>
2016-09-21 17:18:04 +0200

    initial checkin of war to learn jax-rs

---
+++ jaxrs_resteasy/src/main/java/com/stevesouza/jaxrs_resteasy/CustomerApp.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stevesouza.jaxrs_resteasy;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author stevesouza
+ */
+@ApplicationPath("/services")
+public class CustomerApp extends Application {
+    
+}
