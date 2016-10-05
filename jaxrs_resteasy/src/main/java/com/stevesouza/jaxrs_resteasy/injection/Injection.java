526b016220f8798c3e09e8a833637f4a0b68d3ee
Steve Souza <steve@stevesouza.com>
2016-10-05 17:19:53 +0200

    added methods playing with jaxrs injection

--- jaxrs_resteasy/src/main/java/com/stevesouza/jaxrs_resteasy/injection/Injection.java
+++ jaxrs_resteasy/src/main/java/com/stevesouza/jaxrs_resteasy/injection/Injection.java
@@ -3,19 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.stevesouza.jaxrs_resteasy;
+package com.stevesouza.jaxrs_resteasy.injection;
 
-import java.text.MessageFormat;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
+import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.MultivaluedMap;
 
 /**
  * Note this class is instantiated per request by default.  See CustomerApp comments for 
@@ -26,77 +30,29 @@ import javax.ws.rs.core.Response.Status;
  * 
  * can also do 
  * curl  http://localhost:8081/jaxrs/services/customers.json
+ * 
+ * Testing of various jax-rs injection annotations. Note for per request resources these 
+ * annotations may also be used on fields, setters and constructor arguments
+ *  @PathParam
+ *  @MatrixParam
+ *  @QueryParam
+ *  @FormParam
+ *  @HeaderParam
+ *  @CookieParam
+ *  @Context
  *    
  * @author stevesouza
  */
 
 // note I tried to move @Path to the interface and at least for RestEasy this threw an exception
-@Path("/customers")
+@Path("/inject")
 // you can override @Produces per method
 // "application/xml", 
 @Produces({"application/json", "text/plain", "application/xml"})
 @Consumes({"application/json","application/xml"})
-public class CustomerServiceImpl implements CustomerService {
+public class Injection {
     
-    private static CustomerDb  customers = new CustomerDb();
-    
-    /* sample call:
-    http://localhost:8081/jaxrs/services/customers/1
-    */
-    @Override
-    public Map<Integer, Customer> getCustomerList() {
-        return customers.getCustomerList();
-    }
-    
-    /* sample call (takes customer id)
-    http://localhost:8081/jaxrs/services/customers/1
-    */
-    @Override
-    public Customer getCustomer(int id) {
-        return customers.getCustomer(id);
-    }
-    
-    @Override
-    public Response deleteCustomer(int id) {
-      if (customers.contains(id)) {
-         customers.deleteCustomer(id);   
-         return Response.status(Status.NO_CONTENT).build();
-      } else {
-          return Response.noContent().build();
-      }
-    }
-    
-    
-    /** because this method consumes xml and json it can automatically convert them
-     *   to Customer (for xml jaxb tags are required in Customer class).  xml support is part of 
-     * jaxrs whereas you must include jackson for json to work.
-     */
-    @Override
-    public Response createCustomer(Customer customer) {
-        customers.addCustomer(customer);
-        // need to fix this to proper response.
-        // note customer is automatically converted to xml or json based on the http header 'Accept:'
-        // proper response is 200 when content and 204 when not.  
-        return Response.ok().entity(customer).build();
-    }
-    
-    /** update the customer with the given id with any non-empty information from within the passed in
-     * Customer.
-     * 
-     * @param id customer to update
-     * @param customer update with this customer info
-     * @return 
-     */
-    @Override
-    public Customer updateCustomer(int id, Customer customer) {
-        if (customers.updateCustomer(id, customer)) {
-          Customer updatedCustomer = customers.getCustomer(id);
-          return updatedCustomer;
-        }
-        throw new NotFoundException(MessageFormat.format("The customer with id={0} does not exist.", id)); 
-    }
-
-    
+ 
     /** 
      * The @Context annotation allows you to inject instances of 
      * javax.ws.rs.core.HttpHeaders, javax.ws.rs.core.UriInfo, javax.ws.rs.core.Request, 
@@ -108,12 +64,57 @@ public class CustomerServiceImpl implements CustomerService {
      * @return 
      */
     @GET
-    @Path("headers")
+    @Path("/headers")
     public HttpHeaders getHeaders(@Context HttpHeaders headers) {
         return headers;     
     }
+    
+    /**
+     * no difference between the next 2 for the way the method is defined.  you may be able to distinguish by using
+     * the api directly in the method though.
+     * 
+     * curl http://localhost:8081/jaxrs/services/inject/pathparams/steve;age=57/jones
+     * curl http://localhost:8081/jaxrs/services/inject/pathparams/steve/jones;age=57
+     * 
+     * use age default
+     * curl http://localhost:8081/jaxrs/services/inject/pathparams/steve/jones
+     * curl http://localhost:8081/jaxrs/services/inject/pathparams/steve/jones?search=hi
+     * @param fname
+     * @param lname
+     * @param search
+     * @param age
+     * @return 
+     */
+    @GET
+    @Path("/pathparams/{fname}/{lname}")
+    public Map<String, String> getNames(
+            @PathParam("fname") String fname, 
+            @PathParam("lname") String lname,
+            @QueryParam("search") @DefaultValue("jobHistory") String search,
+            @MatrixParam("age") @DefaultValue("99") int age) {
+        
+      Map<String, String> map = new HashMap<>();  
+      map.put("uri", "/pathparams/{fname}/{lname}?search=anything");
+      map.put("fname", fname);
+      map.put("lname", lname);
+      map.put("age", String.valueOf(age));
+      map.put("search", search);
 
+      return map;
+    }
 
+    /**
+     * note you can reuse the same key
+     *  curl http://localhost:8081/jaxrs/services/inject/queryparams?hi=steve&hi=souza&bye=joe
+     * @param uriInfo
+     * @return any query parameters that are submitted
+     */
+    @GET
+    @Path("/queryparams")
+    public MultivaluedMap<String, String> getQueryParameters(@Context UriInfo uriInfo)
+    {
+        return uriInfo.getQueryParameters();
+    }
 
 
 
