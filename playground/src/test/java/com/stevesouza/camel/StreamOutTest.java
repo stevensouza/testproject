404024c69460fe38b55625d585a90c24c5d340ee
Steve Souza <steve@stevesouza.com>
2016-10-30 12:53:14 +0100

    fix imports

--- playground/src/test/java/com/stevesouza/camel/StreamOutTest.java
+++ playground/src/test/java/com/stevesouza/camel/StreamOutTest.java
@@ -5,14 +5,9 @@
  */
 package com.stevesouza.camel;
 
-import org.apache.camel.EndpointInject;
-import org.apache.camel.Message;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.DefaultExchange;
-import org.apache.camel.impl.DefaultMessage;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
