0f9127972254890aee603722272c7942e74c957b
Steve Souza <steve@stevesouza.com>
2016-10-30 12:53:03 +0100

    file route test

---
+++ playground/src/test/java/com/stevesouza/camel/FileListRouteTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stevesouza.camel;
+
+import com.stevesouza.Utils;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author stevesouza
+ */
+@Component
+ public class FileListRouteTest extends CamelTestSupport {
+
+   
+    @Test
+    public void testSysOut()  {
+    }
+
+    private String dir = "file:"+Utils.getInputDir();
+    
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+              from(dir+"?noop=true").to("stream:out");      
+            }
+        };
+    }  
+}
