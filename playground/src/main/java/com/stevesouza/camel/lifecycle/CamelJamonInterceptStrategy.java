9f6e4a0d6865cc37a19ee877b3e243fbbdeaab8b
Steve Souza <steve@stevesouza.com>
2016-11-17 14:00:28 +0100

    added comments to better clarify camel InterceptStrategy

--- playground/src/main/java/com/stevesouza/camel/lifecycle/CamelJamonInterceptStrategy.java
+++ playground/src/main/java/com/stevesouza/camel/lifecycle/CamelJamonInterceptStrategy.java
@@ -10,6 +10,16 @@ import org.apache.log4j.Logger;
 
 /**
  * Created by stevesouza on 5/29/14.
+ * see p 379 of camel in action.  note this is picked up as an intercept strategy as 
+ * part of the spring.xml file. It is defined as a bean in there and automatically loaded
+ * into the Camel context.  you could manually do this by  adding the interceptor to the
+ * camel context: context.addInterceptStrategy(new MyInterceptor());
+ * 
+ * 
+ * 
+ * The effect of an interceptStrategy on the route follows:
+ *   normal route:  consumer->processor1->processor2->producer
+ *   intercepted route: consumer->interpector(proessor1)->interceptor(processor2)->producer
  */
 //@Component this could be defined as a component instead of in the spring context.xml however
 // i think that is a bit more mysterious.
@@ -18,11 +28,16 @@ public class CamelJamonInterceptStrategy implements InterceptStrategy {
 
 
     @Override
-    public Processor wrapProcessorInInterceptors(CamelContext context, ProcessorDefinition<?> definition,
-           final Processor target, Processor nextTarget) throws Exception {
+    public Processor wrapProcessorInInterceptors(
+            CamelContext context, 
+            ProcessorDefinition<?> definition,
+            final Processor target, 
+            Processor nextTarget) throws Exception {
+        
 
         Processor processor = new Processor() {
             public void process(Exchange exchange) throws Exception {
+                // I don't use jamon, but presumably could
                 LOG.info("* Start interceptor - " + target.getClass());
                 target.process(exchange);
                 LOG.info("* End interceptor");
@@ -31,6 +46,6 @@ public class CamelJamonInterceptStrategy implements InterceptStrategy {
 
         };
 
-        return new DelegateAsyncProcessor(processor);
+        return new DelegateAsyncProcessor(processor); // not clear why the processor is wrapped in this but it seems to be standard.
     }
 }
