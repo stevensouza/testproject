ed1d1855db91424774d952fc8a4f78a3091f091d
Steve Souza <steve@stevesouza.com>
2017-01-26 12:54:01 -0500

    changed package

--- playground/src/main/java/com/stevesouza/camel/examples/SimpleRouteBuilder.java
+++ playground/src/main/java/com/stevesouza/camel/examples/SimpleRouteBuilder.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.stevesouza.camel;
+package com.stevesouza.camel.examples;
+
+import com.stevesouza.camel.BaseRouteBuilder;
 
 /**
  *
