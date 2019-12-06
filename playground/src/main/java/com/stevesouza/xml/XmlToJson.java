b7647fa2da4b7c2d86919c5a2c2791997307ccb3
Steve Souza <steve@stevesouza.com>
2019-12-05 20:28:47 -0500

    xml to json changes

--- playground/src/main/java/com/stevesouza/xml/XmlToJson.java
+++ playground/src/main/java/com/stevesouza/xml/XmlToJson.java
@@ -1,11 +1,21 @@
 package com.stevesouza.xml;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.apache.commons.beanutils.PropertyUtils;
 import org.json.JSONObject;
 import org.json.XML;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
 public class XmlToJson {
 
-    public static void main(String[] arg) {
+    public static void main(String[] arg) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException, IOException {
         String xml="<note>\n" +
                 "<to>Tove</to>\n" +
                 "<from>Jani</from>\n" +
@@ -88,5 +98,47 @@ public class XmlToJson {
         System.out.println(jsonObject.getJSONObject("ns2:INVResponseMessage").getJSONObject("ns2:SFSResponse").getJSONObject("ResponseElements").get("SupportRqstId"));
         System.out.println(jsonObject.getJSONObject("ns2:INVResponseMessage").getJSONObject("ns2:SFSResponse").getJSONObject("ResponseElements").get("SupportRqstId").getClass());
 
+        Map<String, Object> jsonMap = jsonObject.toMap();
+        System.out.println(PropertyUtils.getProperty(jsonMap, "ns2:INVResponseMessage.ns2:SFSResponse.ResponseElements.SupportRqstId"));
+        System.out.println(get(jsonMap, "ns2:INVResponseMessage.ns2:SFSResponse.ResponseElements"));
+        Map<String, Object> responseElements = get(jsonMap, "ns2:INVResponseMessage.ns2:SFSResponse.ResponseElements");
+        System.out.println("SupportRqstId="+responseElements.get("SupportRqstId")+", class="+responseElements.get("SupportRqstId").getClass());
+        JsonNode jsonNode = xmlToJson(xml);
+        // note jackson incorrectly converts the xml array of 3 to an object of 1.
+        System.out.println(jsonNode.toPrettyString());
+
+        System.out.println(get(toMap(jsonNode), "SFSResponse.ResponseElements").get("SupportRqstId"));
+
     }
+
+    public static JsonNode xmlToJson(String xml) throws IOException {
+        System.out.println("*** Converting XML to JSON with jackson***");
+
+        // Create a new XmlMapper to read XML tags
+        XmlMapper xmlMapper = new XmlMapper();
+
+        //Reading the XML
+        JsonNode jsonNode = xmlMapper.readTree(xml);
+
+        //Create a new ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //Get JSON as a string
+        String value = objectMapper.writeValueAsString(jsonNode);
+
+        return jsonNode;
+
+    }
+
+    public static Map<String, Object> toMap(JsonNode jsonNode) {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> result = mapper.convertValue(jsonNode, new TypeReference<Map<String, Object>>(){});
+        return result;
+    }
+
+    public static Map<String, Object> get(Map<String, Object> jsonMap, String path) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        return (Map<String, Object>) PropertyUtils.getProperty(jsonMap, path);
+    }
+
 }
+
