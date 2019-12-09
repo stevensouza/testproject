81cb4371de7435e5831202ae4ad64b6724b45aa7
Steve Souza <steve@stevesouza.com>
2019-12-09 11:50:27 -0500

    added comments

--- playground/src/main/java/com/stevesouza/misc/Status.java
+++ playground/src/main/java/com/stevesouza/misc/Status.java
@@ -8,7 +8,7 @@ public enum Status {
     WAITING("W");
 
     private String id;
-    
+
     Status(String id) {
         this.id = id;
     }
@@ -17,10 +17,21 @@ public enum Status {
         return id;
     }
 
+    /**
+     *
+     * @param name case insensitive string comparison to enum's name field.  For example 'accepted' would match Status.ACCEPTED
+     * @return returns the matching Status
+     */
     public static Status toStatus(String name) {
         return Status.valueOf(name.toUpperCase());
     }
 
+
+    /**
+     *
+     * @param name case sensitive name value to see if that string is a Status.name() value.
+     * @return true if the string is a Status enum.
+     */
     public static boolean contains(String name) {
         return Arrays.asList(Status.values()).stream().anyMatch(status -> name.equals(status.name()));
     }
