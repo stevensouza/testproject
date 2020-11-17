f7c90a3618f51c937b7da480a929c0d24353e4f0
Steve Souza <steve@stevesouza.com>
2020-11-17 12:04:49 -0500

    reformatted code

--- playground/src/test/java/com/stevesouza/pentaho/CreateTableDefinition.java
+++ playground/src/test/java/com/stevesouza/pentaho/CreateTableDefinition.java
@@ -4,34 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/** Generate ddl like the following from pentaho field types.  Note with pentaho there is no need to
+/**
+ * Generate ddl like the following from pentaho field types.  Note with pentaho there is no need to
  * create the Field object as it has the equivalent of it in the following
- *
+ * <p>
  * below outputRowMeta = RowMetaInterface
- *   https://github.com/pentaho/pentaho-kettle/blob/master/core/src/main/java/org/pentaho/di/core/row/RowMetaInterface.java
- *
+ * https://github.com/pentaho/pentaho-kettle/blob/master/core/src/main/java/org/pentaho/di/core/row/RowMetaInterface.java
+ * <p>
  * ValueMetaInterface = Field (in my code)
- *   https://github.com/pentaho/pentaho-kettle/blob/master/core/src/main/java/org/pentaho/di/core/row/ValueMetaInterface.java
- *
+ * https://github.com/pentaho/pentaho-kettle/blob/master/core/src/main/java/org/pentaho/di/core/row/ValueMetaInterface.java
+ * <p>
  * List<ValueMetaInterface> list = data.outputRowMeta.getValueMetaList();
  * Same as my List<Field>
- *
+ * <p>
  * CREATE TABLE gsa_fas_finance.tbl_actual_ytd_plan_1 (
- *   `FiscalYear` int(11) DEFAULT NULL,
- *   `AccountingPeriod` int(11) DEFAULT NULL,
- *   `Region` varchar(255) DEFAULT NULL,
- *   `SegmentAgency` varchar(255) DEFAULT NULL,
- *   `SegmentPortfolio` varchar(255) DEFAULT NULL,
- *   `SegmentSummary` varchar(255) DEFAULT NULL,
- *   `SegmentDetail` varchar(255) DEFAULT NULL,
- *   `LineSummarySequence` int(11) DEFAULT NULL,
- *   `LineMidlevelSequence` int(11) DEFAULT NULL,
- *   `SummaryLabel` varchar(255) DEFAULT NULL,
- *   `MidlevelLabel` varchar(255) DEFAULT NULL,
- *   `LineDateLabel` varchar(255) DEFAULT NULL,
- *   `PLLineItem` varchar(255) DEFAULT NULL,
- *   `ActualPlanForecast` varchar(255) DEFAULT NULL,
- *   `Amount` decimal(14,2) DEFAULT NULL
+ * `FiscalYear` int(11) DEFAULT NULL,
+ * `AccountingPeriod` int(11) DEFAULT NULL,
+ * `Region` varchar(255) DEFAULT NULL,
+ * `SegmentAgency` varchar(255) DEFAULT NULL,
+ * `SegmentPortfolio` varchar(255) DEFAULT NULL,
+ * `SegmentSummary` varchar(255) DEFAULT NULL,
+ * `SegmentDetail` varchar(255) DEFAULT NULL,
+ * `LineSummarySequence` int(11) DEFAULT NULL,
+ * `LineMidlevelSequence` int(11) DEFAULT NULL,
+ * `SummaryLabel` varchar(255) DEFAULT NULL,
+ * `MidlevelLabel` varchar(255) DEFAULT NULL,
+ * `LineDateLabel` varchar(255) DEFAULT NULL,
+ * `PLLineItem` varchar(255) DEFAULT NULL,
+ * `ActualPlanForecast` varchar(255) DEFAULT NULL,
+ * `Amount` decimal(14,2) DEFAULT NULL
  * ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
  */
 public class CreateTableDefinition {
@@ -44,10 +45,24 @@ public class CreateTableDefinition {
         this.fields = fields;
     }
 
-    /** Taken a list of fields objects generate table ddl from it. */
+    public static void main(String[] args) {
+        List<Field> fields = new ArrayList<>();
+        fields.add(new Field("fiscalYear", "Number", 15, 2));
+        fields.add(new Field("accountingPeriod", "String", 35, 0));
+        fields.add(new Field("fname", "String", 100, 0));
+        fields.add(new Field("age", "Integer", 35, 0));
+        fields.add(new Field("lname", "XXXnoexist", 155, 0));
+
+        CreateTableDefinition table = new CreateTableDefinition("mytable", fields);
+        System.out.println(table.getTableDefinition());
+    }
+
+    /**
+     * Taken a list of fields objects generate table ddl from it.
+     */
     public String getTableDefinition() {
         final String ROW_SUFFIX = ",\n";
-        return fields.stream().map(f->getFieldDdl(f))
+        return fields.stream().map(f -> getFieldDdl(f))
                 .collect(Collectors.joining(ROW_SUFFIX, getTablePrefix(), getTableSuffix()));
     }
 
@@ -67,6 +82,7 @@ public class CreateTableDefinition {
     public String getFieldDdl(Field field) {
         StringBuilder sb = new StringBuilder("    ")
                 .append("`").append(field.getName()).append("` ");
+
         switch (field.getType()) {
             case "String":
                 sb.append(String.format("varchar(%d)", field.getLength()));
@@ -83,18 +99,5 @@ public class CreateTableDefinition {
         }
 
         return sb.append(" DEFAULT NULL").toString();
-    }
-
-
-    public static void main(String[] args) {
-        List<Field> fields = new ArrayList<>();
-        fields.add(new Field("fiscalYear", "Number", 15, 2));
-        fields.add(new Field("accountingPeriod", "String", 35, 0));
-        fields.add(new Field("fname", "String", 100, 0));
-        fields.add(new Field("age", "Integer", 35, 0));
-        fields.add(new Field("lname", "XXXnoexist", 155, 0));
-
-        CreateTableDefinition table = new CreateTableDefinition("mytable", fields);
-        System.out.println(table.getTableDefinition());
     }
 }
