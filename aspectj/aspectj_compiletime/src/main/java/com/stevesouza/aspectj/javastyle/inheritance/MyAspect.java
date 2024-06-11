2c0b4487d6a90c5a9b5a9a17e584a4c887919e81
Steve Souza <steve@stevesouza.com>
2024-06-11 11:22:36 -0400

    Reviewed and improved comments

--- aspectj/aspectj_compiletime/src/main/java/com/stevesouza/aspectj/javastyle/inheritance/MyAspect.java
+++ aspectj/aspectj_compiletime/src/main/java/com/stevesouza/aspectj/javastyle/inheritance/MyAspect.java
@@ -13,13 +13,20 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class MyAspect  {
 
+    // The following will work for any methods defined in MyClass and children but not any methods
+    // defined only in the children (new methods in children).
+    // Note it will include static methods such as 'main'
+    // Note MyClass works for only MyClass in this package and so the full classpath is
+    // not required.
     @Before("execution(* MyClass.*(..))")
     public void myAdvice1(JoinPoint.StaticPart joinPoint) {
         System.out.println("  MyClass "+joinPoint.getKind()+": "+joinPoint);
     }
 
-    // gets rid of static ie. main()
-    //@Before("execution(* MyClass+.*(..)) && this(MyClass)")
+    // The following will work for both methods defined in MyClass as well as any new methods defined in
+    // its children (MyClass+). Note it will include static methods.
+    // The commented out @Before gets rid of static ie. main()
+    //   @Before("execution(* MyClass+.*(..)) && this(MyClass)")
     @Before("execution(* MyClass+.*(..))")
     public void myAdvice2(JoinPoint.StaticPart joinPoint) {
         System.out.println("  MyClass+ "+joinPoint.getKind()+": "+joinPoint);
